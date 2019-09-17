c739369885f442a8d0aade38645be192f8df77b7
JorgeAMS <jamorales516@icloud.com>
2019-09-17 12:26:29 -0500

    First view and urls path added

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path               # we added include import
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),           #we include urls.py which is inside polls folder
     path('admin/', admin.site.urls),
 ]
