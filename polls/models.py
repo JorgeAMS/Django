7224fac417b1f07f1c5d6b1e753a0c987dffd7c3
JorgeAMS <jamorales516@icloud.com>
2019-09-18 12:38:02 -0500

    Tests added

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Question (models.Model):
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
@@ -16,4 +19,6 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+
 
